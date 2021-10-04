8ade750b98c7b7ea3ace82ea8cb13e0ad5efe096
shireen <syed.shireen@everest.engineering>
2021-10-04 21:50:32 +0530

    test cases modified using mocking

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
-import axios from "axios";
-export default async function mocking(){
-    const response=await axios.get("https://dog.ceo/api/breeds/image/random")
-      return response.data['status']
-      
+
+import "isomorphic-fetch"
+
+export async function mocking() {
+  try {
+    const result = await fetch(
+      `https://dog.ceo/api/breeds/image/random`
+    );
+    const data = await result.json();
+    return data.status
+  } catch (e) {
+    return Promise.reject(e)
+  }
 }
-mocking()
+
+console.log(mocking())
+export default mocking
+//Using Axios here
+// import axios from "axios";
+// export default async function mocking(){
+//     const response=await axios.get("https://dog.ceo/api/breeds/image/random")
+//       return response.data['status']
+      
+// }
+// mocking()
