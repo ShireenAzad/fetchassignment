e1a1136562783befbf1a6c0aea4434b0c262cf06
shireen <syed.shireen@everest.engineering>
2021-10-05 10:43:55 +0530

    Squashed commit of the following:
    commit 070038298a28da340ec7d61f9674b378e66efb5e
    Author: shireen <syed.shireen@everest.engineering>
    Date:   Mon Oct 4 21:52:20 2021 +0530
    
        test cases verified
    
    commit 8ade750b98c7b7ea3ace82ea8cb13e0ad5efe096
    Author: shireen <syed.shireen@everest.engineering>
    Date:   Mon Oct 4 21:50:32 2021 +0530
    
        test cases modified using mocking
    
    commit d9aeaf824676294cadfdc76b648aa4b06e3cfb55
    Author: ShireenAzad <88663880+ShireenAzad@users.noreply.github.com>
    Date:   Mon Oct 4 11:54:45 2021 +0530
    
        Update README.md
    
    commit 12016338e6bf5d052d85e72307fe310ac65352e0
    Author: ShireenAzad <88663880+ShireenAzad@users.noreply.github.com>
    Date:   Mon Oct 4 11:32:53 2021 +0530
    
        Update README.md

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,11 +1,57 @@
-import mocking from "../src/app"
-describe("checking status",function()
+import {mocking} from "../src/app"
+import fetchMock from "jest-fetch-mock";
+jest.mock("../src/app.ts")
+fetchMock.enableMocks();
+describe("api fetched or not",function()
 {
-    test("success ",async function() {
-
-        expect(await mocking()).toBe("success")
+    it("failedcase",async function () {
+        fetchMock.mockReject(() =>
+        mocking().then(() => Promise.reject("failed"))
+        ) 
+       
     })
-//     test("fail",async function() {
-//         expect(await mocking()).toBe("cannot import")
-//     })
-})
+    it("success case",async function () {
+        fetchMock.mockResponse(() => mocking().then(() => 'success'))
+        
+    })
+})   
+
+//Fetching manually without mocking by using Try catch
+// describe("checking status",function()
+// {
+//     test("checking the status whether  pass or fail", async function () 
+        
+//     {
+        
+//         try {
+//             const solution=await mocking()
+//         expect(solution).toBe("success")
+//         console.log("success")
+//         } catch (e) {
+//           expect(e).toMatch('null');
+//           console.log("Failed")
+//         }
+//       });
+// })
+
+//Using fetch ,mock implementation by resolving promises 
+  
+//   it('success test case', async function ()  {
+//     const mockSuccessResponse = await fetch( `https://dog.ceo/api/breeds/image/random`);
+//     const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+//     const mockFetchPromise = Promise.resolve({
+//         json: () => mockJsonPromise,
+//     });
+//     var globalRef:any =global;
+//     globalRef.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+
+    
+
+//     mocking();
+
+//   });
+//   it('failed test case', async function ()  {
+//     jest.fn().mockImplementationOnce(() => Promise.reject('null'));
+//     mocking();
+
+//   });
